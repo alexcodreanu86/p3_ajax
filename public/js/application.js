a7f54a7bc84eb7e8da448d794a07328bfc1f6e24
Alexandru Codreanu <alexcodreanu86@yahoo.com>
2014-02-27 09:39:28 -0600

    ajaxified with colors

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-
+  $("#get_color").on('click',function(e){
+    var data;
+    $.post('/color',data, function(response){
+      $('#' + response.cell).attr('style', 'background-color: ' + response.color + ";");
+    },"json");
+  })
 });
